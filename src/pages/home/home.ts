ebd79872535b036f96cedf72235101cc1e4ca2a5
Rich Vicker <richvicker@hotmail.com>
2018-06-25 18:07:26 -0500

    test code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  runcount: string = '';
+  items = [];
+  individuals = [];
+  toDoItem:string='';
 
+  constructor(public navCtrl: NavController, public http:Http) {
+    this.runcount = 'Gabe'
+    this.http.get('http://test.vickerhome.com/api/test/individuals').map(res => res.json()).subscribe(data => {
+        this.individuals = data;
+    });
   }
 
+  addToItems(){
+    this.items.push(this.toDoItem);
+  }
 }
