d720ce56a7d81991b3fcd56f1c781a95607619f5
gvicker <gabe@vickerhome.com>
2018-07-02 11:08:11 -0500

    Tested out teams and locations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,13 +14,21 @@ export class HomePage {
   items = [];
   individuals = [];
   toDoItem:string='';
-  terrains = [];
+  //terrains = [];
+  //teams = [];
+  locations = [];
 
   constructor(public navCtrl: NavController, public http:Http) {
     this.runcount = 'Gabe'
     
-    this.http.get('http://test.vickerhome.com/api/terrains/get').map(res => res.json()).subscribe(data => {
+    /*this.http.get('http://test.vickerhome.com/api/terrains/get').map(res => res.json()).subscribe(data => {
         this.terrains = data;
+    });
+    this.http.get('http://test.vickerhome.com/api/teams/get').map(res => res.json()).subscribe(data => {
+        this.teams = data;
+    });*/
+    this.http.get('http://test.vickerhome.com/api/locations/get').map(res => res.json()).subscribe(data => {
+        this.locations = data;
     });
   }
 
