afed954a2fba1ea8c165aee2b86a77486fbbc3ac
Rich Vicker <richvicker@hotmail.com>
2018-09-04 19:12:56 -0500

    minor nav changes

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 //import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+//import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { GroupTimerPage } from '../group-timer/group-timer'
 
@@ -13,7 +13,7 @@ export class TabsPage {
   tab1Root = HomePage;
   //tab2Root = AboutPage;
   tab2Root = GroupTimerPage
-  tab3Root = ContactPage;
+  //tab3Root = ContactPage;
 
   constructor() {
 
